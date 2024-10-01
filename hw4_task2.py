762d0363dabc16ccac8c1acc7269948df7158f65
nataliehill <natalie.a.hill-1@ou.edu>
2024-10-01 13:59:33 -0500

    Completed task 2

--- hw4_task2.py
+++ hw4_task2.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 # import necessary library 
 # if you need more libraries, just import them
 from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
 # ......
 # --- end of task --- #
 
@@ -12,7 +13,7 @@ from sklearn.linear_model import Ridge
 # in array "data", each row represents a community 
 # each column represents an attribute of community 
 # last column is the continuous label of crime rate in the community
-data = np.loadtxt('crimerate.csv', delimiter=',', skiprows=1)
+data = np.loadtxt('data/crimerate.csv', delimiter=',', skiprows=1)
 [n,p] = np.shape(data)
 
 # always use last 25% data for testing 
@@ -25,7 +26,7 @@ label_test = data[n-num_test:,-1]
 # now, pick the percentage of data used for training 
 # remember we should be able to observe overfitting with this pick 
 # note: maximum percentage is 0.75 
-per = ...
+per = 0.75   
 num_train = int(n*per)
 sample_train = data[0:num_train,0:-1]
 label_train = data[0:num_train,-1]
@@ -38,7 +39,7 @@ label_train = data[0:num_train,-1]
 # Pick 8 candidate values for alpha (in ascending order)
 # Remember we should aim to observe both overfitting and underfitting from these values 
 # Suggestion: the first value should be very small and the last should be large 
-alpha_vec = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+alpha_vec = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
 # --- end of task --- #
 
 er_train_alpha = []
@@ -50,15 +51,15 @@ for alpha in alpha_vec:
     
     # --- Your Task --- #
     # now train your model using (sample_train, label_train)
-    # ......
-    # ......
+    model.fit(sample_train, label_train)
     # now evaluate your training error (MSE) and stores it in "er_train"
-    # ......
-    # ......
+    predictions_train = model.predict(sample_train)
+    er_train = mean_squared_error(label_train, predictions_train)
+
     er_train_alpha.append(er_train)
     # now evaluate your testing error (MSE) and stores it in "er_test"
-    # ......
-    # ......
+    predictions_test = model.predict(sample_test)
+    er_test = mean_squared_error(label_test, predictions_test)
     er_test_alpha.append(er_test)
     # --- end of task --- #
 
@@ -68,6 +69,4 @@ plt.plot(alpha_vec,er_test_alpha, label='Testing Error')
 plt.xlabel('Hyper-Parameter Alpha')
 plt.ylabel('Prediction Error (MSE)')
 plt.legend()
-    
-
-
+plt.show()
